dce5d945fceb9aadb63eb2e77ecf65fa13706fa1
NatalyaBolshakova <61007035+NatalyaBolshakova@users.noreply.github.com>
2020-03-21 13:27:39 +0400

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array === undefined || array == ''){
+        return 0;
+      } else {
+        let minR = array.reduce((minN, item) => Math.min(minN,item),0);
+        return minR;
+      }    
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array === undefined || array == ''){
+        return 0;
+      } else {
+        let maxR = array.reduce((maxN, item) => Math.max(maxN,item),0);
+        return maxR;
+      }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array === undefined || array == ''){
+        return 0;
+      } else {
+        let sumR = array.reduce((sum, item) => sum + item,0);
+        let avgR = sumR/array.length;
+        return avgR;
+      }
 }
